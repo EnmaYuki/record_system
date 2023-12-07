3db23e2db0b66f7b5dc9c1198b97d65858f11724
Elayne <enmayuki5@gmail.com>
2023-12-07 23:41:49 +0800

    user id

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             <div class="row">
                 <div class="col-12">
                     <form action="login.php" method="POST">
-                        <label for="username">Username:</label>
+                        <label for="username">User ID:</label>
                         <input type="text" id="username" name="username" required><br><br>
                         <label for="password">Password:</label>
                         <input type="password" id="password" name="password" required><br><br>
